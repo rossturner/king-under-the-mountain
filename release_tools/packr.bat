99f4830ef6a9e2624a2014909e035b6145d2f5b3
Ross Taylor-Turner <ross.turner001@googlemail.com>
2022-02-12 15:01:09 +0000

    Alpha 8.1.19 release (seems to be having Java issues)

--- release_tools/packr.bat
+++ release_tools/packr.bat
@@ -3,11 +3,11 @@
 @echo:
 @echo Packaging win build
 rm -rf win64_release
-java -jar packr.jar win-config.json
+java -jar packr-all-4.0.0.jar win-config.json
 rcedit-x64 "win64_release/King under the Mountain.exe" --set-icon "icon/favicon.ico"
 @echo Packaging mac build
 rm -rf macosx_release
-java -jar packr.jar mac-config.json
+java -jar packr-all-4.0.0.jar mac-config.json
 @echo Packaging linux build
 rm -rf linux64_release
-java -jar packr.jar linux-config.json
+java -jar packr-all-4.0.0.jar linux-config.json
--- release_tools/packr.sh
+++ release_tools/packr.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 rm -rf win64_release
-java -jar packr.jar win-config.json
+java -jar packr-all-4.0.0.jar win-config.json
 rm -rf macosx_release
-java -jar packr.jar mac-config.json
+java -jar packr-all-4.0.0.jar mac-config.json
 rm -rf linux64_release
-java -jar packr.jar linux-config.json
+java -jar packr-all-4.0.0.jar linux-config.json
